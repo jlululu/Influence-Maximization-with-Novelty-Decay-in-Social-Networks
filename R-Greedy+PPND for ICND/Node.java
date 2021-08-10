38d452d4c6eb57b0f59dd1c52c97574c92adf7f3
jlululu <79313033+jlululu@users.noreply.github.com>
2021-08-10 14:25:01 +0100

    Update and rename R-Greedy/Novelty_decay.py to R-Greedy+PPND for ICND/Node.java

---
+++ R-Greedy+PPND for ICND/Node.java
@@ -0,0 +1,27 @@
+package IMND.IC
+
+import java.util.*;
+
+public class Node{
+    int label, indegree, outdegree;
+    double threshold;
+    List<Integer>successor;
+    List<Integer>predecessor;
+
+    Node(){
+		this.label = -1;
+	}
+
+    Node(int label, List<Integer> successor, List<Integer> predecessor){
+      Random rand = new Random();
+      this.label = label;
+      this.indegree = predecessor.size();
+      this.outdegree = successor.size();
+	  // this.threshold = 0.8;
+      this.threshold = 1-rand.nextDouble()*0.9;
+      this.successor = successor;
+      this.predecessor = predecessor;
+    }
+    
+    public static void main(String[] args){}
+}
