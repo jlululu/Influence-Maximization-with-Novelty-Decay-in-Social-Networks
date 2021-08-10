dd8637a5716418c904b820b8967a1144cb60ba53
jlululu <79313033+jlululu@users.noreply.github.com>
2021-08-10 15:25:37 +0100

    Update Test.java

--- R-Greedy+PPND for ICND/Test.java
+++ R-Greedy+PPND for ICND/Test.java
@@ -14,11 +14,11 @@ public public class Test{
     int C = 5;
     double theta = 0.001, sigma = 0;
     Set<Node> V = G.nodes;
-	  Map<Integer,Node> M = G.m;
+    Map<Integer,Node> M = G.m;
     Map<List<Node>,Edge> E = G.edges;
     Map<Node,Integer> Count = new HashMap<Node,Integer>();
     Map<Node,List<Double>> PPND = new HashMap<Node,List<Double>>();
-	  Map<Node,List<Double>> Pcon = new HashMap<Node,List<Double>>();
+    Map<Node,List<Double>> Pcon = new HashMap<Node,List<Double>>();
     PriorityQueue<queueNode> PH = new PriorityQueue<queueNode>(new Comparator<queueNode>(){
       @Override
       public int compare(queueNode o1, queueNode o2){
@@ -26,18 +26,18 @@ public public class Test{
       }
     });
     for(Node s : S){
-	    Set<Node> visited = new HashSet<Node>();
+      Set<Node> visited = new HashSet<Node>();
       visited.add(s);
       PH.offer(new queueNode(0,1.0,new Node(),s,visited));
     }
     while(!PH.isEmpty()){
       queueNode q = PH.poll();
       int time = q.time;
-	    double pcon = q.p_con;
-	    Set<Node> path = q.path;
+      double pcon = q.p_con;
+      Set<Node> path = q.path;
       Node u = q.from_node;
-	    Node w = q.to_node;
-	    int t = Count.getOrDefault(w,0);
+      Node w = q.to_node;
+      int t = Count.getOrDefault(w,0);
       if(t >= C)
         continue;
       Edge v = new Edge();
@@ -46,7 +46,7 @@ public public class Test{
         v = E.get(Arrays.asList(u,w));
         inf = (v.weight)*pcon*ND(t,Pcon.getOrDefault(w,new ArrayList<Double>()),0,0,1.0);
       }
-	    if(inf >= theta){
+      if(inf >= theta){
         Count.put(w,t+1);
         List<Double> list1 = Pcon.getOrDefault(w,new ArrayList<Double>());
         list1.add(pcon);
@@ -57,27 +57,27 @@ public public class Test{
         for(int ind : w.successor){
           Node next = M.get(ind);
           if(path.contains(next))
-          continue;
+            continue;
           Edge v_next = E.get(Arrays.asList(w,next));
           Set<Node> path_new = new HashSet<Node>();
           path_new.addAll(path);
           path_new.add(next);
           PH.offer(new queueNode(time+v_next.time,inf,w,next,path_new));
         }
-	    }
+      }
     }
     for(List<Double> list : PPND.values()){
       double pro = 1.0;
       for(double k : list)
-      pro *= (1-k);
+      	 pro *= (1-k);
       sigma += 1-pro;
     }
-      return sigma;
+    return sigma;
   }
 
 //   R-Greedy + PPND
   public static Set<Node> R_Greedy_IC(DiGraph G, int k, Set<Node> S_prev1, Set<Node> visited1, Map<Node,Double> Q1, Map<Node,Double> Q_prev1, TreeSet<Node> Q_list1){
-	  Map<Integer,Node> M = G.m;
+    Map<Integer,Node> M = G.m;
     Set<Node> S_prev = new HashSet<Node>();
     Set<Node> visited = new HashSet<Node>();
     Map<Node,Double> Q = new HashMap<Node,Double>();
@@ -102,7 +102,7 @@ public public class Test{
           break;
         if(visited.contains(u) && (Q_prev.get(u)+Q.getOrDefault(s_prev,0.0)-sigma_prev<maxMarInf))
           continue;
-	      vis.add(u);
+        vis.add(u);
         Set<Node> set = new HashSet<Node>();
         set.addAll(S_prev);
         set.add(u);
@@ -128,22 +128,22 @@ public public class Test{
   // Degree_ND
   public static Set<Node> Degree(DiGraph G, int k){
     Set<Node> re = new HashSet<Node>();
-	Set<Node> V = G.nodes;
-	Map<Integer,Node> M = G.m;
-	Map<Node,Integer> Degree = new HashMap<Node,Integer>();
-	for(Node v : V)
-	  Degree.put(v,v.successor.size()+v.predecessor.size());
+    Set<Node> V = G.nodes;
+    Map<Integer,Node> M = G.m;
+    Map<Node,Integer> Degree = new HashMap<Node,Integer>();
+    for(Node v : V)
+      Degree.put(v,v.successor.size()+v.predecessor.size());
     List<Map.Entry<Node,Integer>> list = new ArrayList<Map.Entry<Node,Integer>>(Degree.entrySet());
-	  Collections.sort(list,new Comparator<Map.Entry<Node,Integer>>(){
-	    @Override
-	    public int compare(Map.Entry<Node,Integer> o1, Map.Entry<Node,Integer> o2){
-		  return o2.getValue()-o1.getValue();
-	    }
-	  });
-	for(int i = 0;i < k;i ++){
-	  Node s = list.get(i).getKey();
-	  re.add(s);
-	}
+    Collections.sort(list,new Comparator<Map.Entry<Node,Integer>>(){
+      @Override
+      public int compare(Map.Entry<Node,Integer> o1, Map.Entry<Node,Integer> o2){
+      return o2.getValue()-o1.getValue();
+      }
+    });
+    for(int i = 0;i < k;i ++){
+      Node s = list.get(i).getKey();
+      re.add(s);
+    }
     return re;
   }
  
@@ -169,7 +169,7 @@ public public class Test{
       int time = curr.time;
       Node u = curr.from_node;
       Node w = curr.to_node;
-        int t = Count.getOrDefault(w,0);
+      int t = Count.getOrDefault(w,0);
       double inf = 1.0;
       if(u.label != -1)
         inf = E.get(Arrays.asList(u,w)).weight*Math.pow(0.3,t);
@@ -192,8 +192,8 @@ public public class Test{
   }
   
 //   R-Greedy + MC
-    public static Set<Node> R_Greedy_IC1(DiGraph G, int k, Set<Node> S_prev1, Set<Node> visited1, Map<Node,Double> Q1, Map<Node,Double> Q_prev1, TreeSet<Node> Q_list1){
-	  Map<Integer,Node> M = G.m;
+  public static Set<Node> R_Greedy_IC1(DiGraph G, int k, Set<Node> S_prev1, Set<Node> visited1, Map<Node,Double> Q1, Map<Node,Double> Q_prev1, TreeSet<Node> Q_list1){
+    Map<Integer,Node> M = G.m;
     Set<Node> S_prev = new HashSet<Node>();
     Set<Node> visited = new HashSet<Node>();
     Map<Node,Double> Q = new HashMap<Node,Double>();
@@ -218,7 +218,7 @@ public public class Test{
           break;
         if(visited.contains(u) && (Q_prev.get(u)+Q.getOrDefault(s_prev,0.0)-sigma_prev<maxMarInf))
           continue;
-	      vis.add(u);
+	vis.add(u);
         Set<Node> set = new HashSet<Node>();
         set.addAll(S_prev);
         set.add(u);
@@ -231,11 +231,11 @@ public public class Test{
       }
       if(s_curr.label == -1)
         break;
-	    Q_list.remove(s_curr);
+      Q_list.remove(s_curr);
       S_prev.add(s_curr);
       sigma_prev += maxMarInf;
       visited = new HashSet<Node>();
-	    visited.addAll(vis);
+      visited.addAll(vis);
       s_prev = s_curr;
     }
     return S_prev;
@@ -243,7 +243,7 @@ public public class Test{
 
   public static void main(String[] args)throws IOException , FileNotFoundException{
     // read in 
-	  Scanner sc = new Scanner(new File("wiki.txt"));
+    Scanner sc = new Scanner(new File("wiki.txt"));
     int len = sc.nextInt();
     Set<Integer> set = new HashSet<Integer>();
     int[][] arr = new int[len][2];
@@ -255,8 +255,8 @@ public public class Test{
     }
     DiGraph G = new DiGraph(set,arr);
 	
-	  Set<Node> V = G.nodes;
-	// initialize for R_Greedy_IC
+    Set<Node> V = G.nodes;
+// initialize for R_Greedy_IC
     Set<Node> S_prev1 = new HashSet<Node>();
     Set<Node> visited1 = new HashSet<Node>();
     Map<Node,Double> Q1 = new HashMap<Node,Double>();
@@ -270,13 +270,13 @@ public public class Test{
     for(Node v : V){
       Set<Node> s = new HashSet<Node>();
       s.add(v);
-      double temp = PPND_Inf_LT(G,s);
+      double temp = PPND_Inf_IC(G,s);
       Q1.put(v,temp);
       visited1.add(v);
       Q_prev1.put(v,temp);
-	    Q_list1.add(v);
+      Q_list1.add(v);
     }
-	// initialize for R_Greedy_IC1
+// initialize for R_Greedy_IC1
     Set<Node> S_prev2 = new HashSet<Node>();
     Set<Node> visited2 = new HashSet<Node>();
     Map<Node,Double> Q2 = new HashMap<Node,Double>();
@@ -290,24 +290,24 @@ public public class Test{
     for(Node v : V){
       Set<Node> s = new HashSet<Node>();
       s.add(v);
-      double temp = Inf_LT(G,s);
+      double temp = IC(G,s);
       Q2.put(v,temp);
       visited2.add(v);
       Q_prev2.put(v,temp);
-	    Q_list2.add(v);
+      Q_list2.add(v);
     }
 	
-    System.out.println("R_Greedy_LT:");
+    System.out.println("R_Greedy_IC:");
     for(int k = 10;k <= 100;){
       long start1 = System.currentTimeMillis();
-        Set<Node> R_seed = R_Greedy_IC(G,k,S_prev1,visited1,Q1,Q_prev1,Q_list1);
+      Set<Node> R_seed = R_Greedy_IC(G,k,S_prev1,visited1,Q1,Q_prev1,Q_list1);
       long elapsedTimeMillis1 = System.currentTimeMillis()-start1;
-        double inf_R = IC(G,R_seed);
+      double inf_R = IC(G,R_seed);
       System.out.println("time="+elapsedTimeMillis1/1000F+"seed number="+R_seed.size()+"inf="+inf_R);
       k += 10;
     }
     
-    System.out.println("R_Greedy_LT1:");
+    System.out.println("R_Greedy_IC1:");
     for(int k = 10;k <= 100;){
       long start2 = System.currentTimeMillis();
       Set<Node> R_seed = R_Greedy_IC1(G,k,S_prev2,visited2,Q2,Q_prev2,Q_list2);
